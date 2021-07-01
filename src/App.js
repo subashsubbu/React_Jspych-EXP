05fc7f0f272ca2a13562d04dc2d811b0d9bb5141
subashsubbu <35239091+subashsubbu@users.noreply.github.com>
2021-07-01 11:20:14 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Experiment } from 'jspsych-react';
+import { timelineFactory } from './timeline';
+import callbackImageKeyboardResponsePlugin from './callbackImageKeyboardResponsePlugin';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const callback = (targetID) => console.log(targetID);
+    const timeline = timelineFactory(callback);
+    return (
+        <div>
+            <Experiment
+                settings={{ timeline }}
+                plugins={{ callbackImageKeyboardResponsePlugin }}
+            />
 
-export default App;
+        </div>
+    )
+}
+export default App
+
---
+++ src/timeline.js
@@ -0,0 +1,115 @@
+import { jsPsych } from 'jspsych-react'
+import callbackImageKeyboardResponsePlugin from "./callbackImageKeyboardResponsePlugin.js";
+
+// Helper function
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+// }
+
+const post_trial_gap = function () {
+  return Math.floor(Math.random() * jitter) + iti;
+};
+
+
+// Experiment parameters
+const stim_duration = 200;
+const iti = 300;
+const jitter = 200;
+const plugin_name = callbackImageKeyboardResponsePlugin;
+
+export function timelineFactory(callback) {
+
+  const start_callback = function () {
+    callback('start')
+  };
+  const stop_callback = function () {
+    callback('stop')
+  };
+
+  // jspsych is in the node_modules
+  // const base_path = '/src/images/';
+  let targets = [
+
+  ];
+
+  var imageNumber;
+  var fileName;
+  var toPush;
+  for (var i = 0; i < 10; i++) {
+    imageNumber = Math.floor(Math.random() * (901 - 1) + 1);
+
+    if (imageNumber <= 9) {
+      fileName = "static/00000" + imageNumber + ".jpg";
+      toPush = { 'stimulus': fileName };
+    }
+    else if (imageNumber > 9 && imageNumber <= 99) {
+      fileName = "static/0000" + imageNumber + ".jpg";
+      toPush = { 'stimulus': fileName };
+    }
+    else {
+      fileName = "static/000" + imageNumber + ".jpg";
+      toPush = { 'stimulus': fileName };
+    }
+    if (targets.includes(toPush)) {
+      i -= 1;
+    }
+    targets.push(toPush);
+  }
+
+  // Create timeline
+  const timeline = [];
+
+  const welcome_block = {
+    type: 'callbackImageKeyboardResponsePlugin',
+    stimulus: "In this exercise, you will be shown a series of quickly moving pictures. In between each picture, you will see a <strong>+</strong>. Please focus on this when there is not an image on the screen. This exercise contains imagery that some may find disturbing. User beware. Press any key to begin.",
+    post_trial_gap: 5000,
+    on_start: start_callback
+  };
+  timeline.push(welcome_block);
+
+  const test_trials = {
+    stimulus: 'stimulus',
+    type: plugin_name,
+    timeline: targets,
+    trial_duration: function () {
+      return jsPsych.randomization.sampleWithoutReplacement([250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750], 1)[0];
+    },
+    stimulus_duration: stim_duration,
+    post_trial_gap: post_trial_gap(),
+    on_start: function () {
+      console.log(this)
+    }
+
+  };
+
+  timeline.push(test_trials);
+
+  // const fixation = {
+  //   type: "callbackImageDisplay",
+  //   stimulus: "./images/fixation.png",
+  //   stimulus_duration: stim_duration,
+  //   trial_duration: () => params.iti + Math.random() * params.jitter,
+  //   post_trial_gap: post_trial_gap(),
+
+  // }
+  // timeline.push(fixation);
+
+  const end_block = {
+    type: 'callbackImageKeyboardResponsePlugin',
+    stimulus: "Thanks for participating!",
+    post_trial_gap: 500,
+    on_start: stop_callback
+  };
+
+  timeline.push(end_block);
+
+  return timeline;
+
+}
+
+
+
+
+
